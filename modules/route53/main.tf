800f6baf68558b648071b9366045d1a78531bbae
Royalboe <auomidejohnson@gmail.com>
2023-02-16 17:58:32 +0000

    changed route zone to public

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,20 +1,17 @@
 # Create a Route53 Zone
 resource "aws_route53_zone" "my_zone" {
   name = var.domain
-  vpc {
-    vpc_id = var.vpc_id
-  }
   tags = var.proj-tag
 }
 
 # Create a Route53 Record
 resource "aws_route53_record" "terraform-test" {
   zone_id = aws_route53_zone.my_zone.zone_id
-  name    = "terraform-test.royalboe.live"
+  name    = "terraform-test.${var.domain}"
   type    = "A"
   alias {
     name                   = var.lb_dns_name
     zone_id                = var.alb_zone_id
-    evaluate_target_health = false
+    evaluate_target_health = true
   }
 }
